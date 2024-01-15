a08e33ddc5e7b6783b22e2bb289a930c7a344302
Tomgtech <tom.gendall@yahoo.com>
2024-01-15 13:34:12 +0000

    jsjsjsjs

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+console.log(blogPosts);
+
+const favouriteFoods = ["Pizza", "Mango", "Dates"];
+console.log(favouriteFoods);
+
+const favouriteColours = ["Green", "Black", "Blue"];
+console.log(favouriteColours);
+
+const favouriteNumbers = ["2", "199", "2020"];
+console.log(favouriteNumbers);
